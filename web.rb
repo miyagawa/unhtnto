eae4456dec2a9c623f489b63bd678a2124131fc9
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-06-26 18:48:01 -0700

    use meta refresh

--- web.rb
+++ web.rb
@@ -3,9 +3,9 @@ require 'net/http'
 require 'uri'
 
 get '/:code' do
-  uri = redirect_uri(params[:code])
-  if uri
-    redirect uri
+  @uri = redirect_uri(params[:code])
+  if @uri
+    erb :redirect
   else
     404
   end
@@ -15,8 +15,18 @@ def redirect_uri(code)
   uri = URI.parse "http://htn.to/#{code}"
   response = Net::HTTP.get_response(uri)
   if [301, 302].include?(response.code.to_i)
-    response['Location'].sub %r[^http://b\.hatena\.ne\.jp/entry/], ''
+    response['Location'].sub %r[^http://b\.hatena\.ne\.jp/entry/], 'http://'
   else
     nil
   end
 end
+
+__END__
+
+@@ redirect
+<html>
+  <head>
+  <meta http-equiv="refresh" content="0; url=<%= @uri %>">
+  </head>
+</html>
+
